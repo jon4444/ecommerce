df38bc620c57e6cf939fc0c05fefb25695d62165
jon4444 <50190069+jon4444@users.noreply.github.com>
2023-06-30 16:18:56 +0100

    shopping basket session

--- store/tests/test_views.py
+++ store/tests/test_views.py
@@ -1,23 +1,42 @@
-# from django.test import TestCase
+from importlib import import_module
 from unittest import skip
-from django.test import TestCase
+
+from django.conf import settings
 from django.contrib.auth.models import User
+from django.http import HttpRequest
+from django.test import Client, TestCase
+from django.urls import reverse
+
 from store.models import Category, Product
 from store.views import product_all
 
-@skip("demonstarting skipping")
+
+@skip("demonstrating skipping")
 class TestSkip(TestCase):
-    def test_skip_example(self):
+    def test_skip_exmaple(self):
         pass
 
 
 class TestViewResponses(TestCase):
     def setUp(self):
         self.c = Client()
+        User.objects.create(username='admin')
+        Category.objects.create(name='django', slug='django')
+        Product.objects.create(category_id=1, title='django beginners', created_by_id=1,
+                               slug='django-beginners', price='20.00', image='django')
 
     def test_url_allowed_hosts(self):
         """
         Test allowed hosts
+        """
+        response = self.c.get('/', HTTP_HOST='noaddress.com')
+        self.assertEqual(response.status_code, 400)
+        response = self.c.get('/', HTTP_HOST='yourdomain.com')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_url(self):
+        """
+        Test homepage response status
         """
         response = self.c.get('/')
         self.assertEqual(response.status_code, 200)
@@ -26,9 +45,8 @@ class TestViewResponses(TestCase):
         """
         Test category response status
         """
-        response= self.c.get(
-            reverse('store:category_list', args=['django'])
-        )
+        response = self.c.get(
+            reverse('store:category_list', args=['django']))
         self.assertEqual(response.status_code, 200)
 
     def test_product_detail_url(self):
@@ -36,8 +54,7 @@ class TestViewResponses(TestCase):
         Test items response status
         """
         response = self.c.get(
-            reverse('store:product_detail', args=['the-road-to-react'])
-        )
+            reverse('store:product_detail', args=['django-beginners']))
         self.assertEqual(response.status_code, 200)
 
     def test_homepage_html(self):
@@ -45,19 +62,10 @@ class TestViewResponses(TestCase):
         Example: code validation, search HTML for text
         """
         request = HttpRequest()
+        engine = import_module(settings.SESSION_ENGINE)
+        request.session = engine.SessionStore()
         response = product_all(request)
         html = response.content.decode('utf8')
         self.assertIn('<title>BookStore</title>', html)
-        self.assertTrue(html.startsWith('\n<!DOCTYPE html>\n'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_view_function(self):
-        """
-        Example: Using request factory
-        """
-        request= self.factory.get('/the-temple-of-django-database-performance')
-        response = product_all(request)
-        html = response.content.decode('utf8')
-        self.assertIn('<title>BookStore </title>', html)
-        self.assertTrue(html.startsWith('\n<!DOCTYPE html>\n'))
+        self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
         self.assertEqual(response.status_code, 200)
